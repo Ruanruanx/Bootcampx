5af4a0ef4ac8c95cfc20a09abbb276d6fd301e1c
Ruanruanx <zxin7@uwo.ca>
2022-09-24 09:22:24 +0000

    Group By

---
+++ 3_queries_group_by/avg_duration.sql
@@ -0,0 +1,12 @@
+SELECT
+  students.name as student,
+  AVG(duration) as average_assignment_duration
+FROM
+  assignment_submissions
+  JOIN students ON students.id = student_id
+WHERE
+  students.end_date IS NULL
+GROUP BY
+  students.name
+ORDER BY
+  average_assignment_duration DESC;
