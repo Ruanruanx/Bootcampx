371efaff4778f496e1957059808c3f0d315503eb
Ruanruanx <zxin7@uwo.ca>
2022-10-02 08:25:44 +0000

    changed = to LIKE

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -13,7 +13,7 @@ FROM assistance_requests
 JOIN students ON students.id=student_id
 JOIN teachers ON teachers.id=teacher_id
 JOIN cohorts ON cohorts.id=cohort_id
-WHERE cohorts.name = $1
+WHERE cohorts.name LIKE $1
 ORDER BY teachers.name;
 `;
 
